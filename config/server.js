b56b857d8b5202c9d6ad12893a80d60cf5b47a30
HamoudBalawi <mohammedahmed87@icloud.com>
2022-09-07 02:57:32 +0200

    update

--- config/server.js
+++ config/server.js
@@ -1,7 +1,11 @@
 module.exports = ({ env }) => ({
+  url: env("https://stays-strapi-api.herokuapp.com"),
+});
+
+/*module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
-});
+});*/
